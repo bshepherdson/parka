d42bb2047285eb3004c3a0a4d83fedf7c1dea0ec
Braden Shepherdson <braden.shepherdson@gmail.com>
2022-02-21 14:18:29 -0500

    Polishing the JSON parser, adding string escapes
    It's too slow on the 7MB benchmark payload for it even to finish...

---
+++ README.md
@@ -0,0 +1,107 @@
+# Parka - Parser Combinators in Clojure
+
+Parka is a [parser combinator](https://en.wikipedia.org/wiki/Parser_combinator)
+library that aims to be both *simple* and *easy*.
+
+That comes at a cost, mostly a performance cost. If you're looking to parse
+large files, you're in the wrong place.
+
+## Usage Guide
+
+Parsing works in two phases: constructing and parsing. The combinators are
+functions that return parsers (which are just data structures).
+
+At the top level a *grammar* is a map of keywords to parsers, plus the starting
+keyword. These are comparable to the *production rules* of an EBNF grammar.
+
+Parsing a complete input begins from a start parser, by default called `:start`.
+
+An example will go a long way:
+
+```clojure
+(ns my.parser
+  (:require
+    [parka.core :as p]))
+
+(def rules
+  {:number     (p/many1 (p/span \0 \9))
+   :subexpr    (p/alt (p/between "(" ")" :expr)
+                      :number)
+   :expr2      (p/pseq :subexpr (p/many (p/pseq :mul-op :subexpr)))
+   :expr       (p/pseq :expr2   (p/many (p/pseq :add-op :expr2)))
+   :add-op     (p/alt "+" "-")
+   :mul-op     (p/one-of "*/")})
+
+(def grammar (p/grammar rules :expr))
+
+(p/parse-str grammar "myfile.txt" "12+6*9")
+```
+
+- As noted above, a grammar is simply a map of rule names to parsers.
+- A `:number` is a list of digit characters.
+    - `(p/span lo hi)` matches any single character between `lo` and `hi`
+      inclusive.
+    - `(p/many1 q)` parses 1 or more repetitions of `q`, returning a list.
+    - So for example `"123"` would parse as `[\1 \2 \3]`.
+    - See below for a way to improve that output.
+- `:subexpr` is a parenthesized `:expr` or a `:number`.
+- Note the two special cases:
+  - A bare string is equivalent to `(p/lit "...")`.
+  - A bare keyword is equivalent to `(p/sym :keyword)`, referencing a rule of
+    the grammar.
+- `:expr2` is a `:subexpr` followed by 0 or more `[:mul-op :subexpr]` pairs.
+  - Eg. `"3*4/2"` parses as an `:expr2` with `[[\3] [[\* \4] [\/ \2]]]`.
+- `:expr` is like `:expr2`, for `:add-op`s.
+- `:add-op` use `p/alt` and `:mul-op` `p/one-of`, but the result is the same:
+  matching one character operators.
+
+Note that there's no code generation, macros, or other magic here - the
+combinators are simply functions that return data structures.
+
+
+### Whitespace
+
+There's no special handling for whitespace - parse it and discard it. Using
+`p/pseq-at` is handy here - there's often only one real value surrounded by
+punctuation and whitespace. We might make the above `:subexpr` more flexible
+with
+
+```clojure
+  :ws      (p/many (p/one-of " \t\r\n"))
+  :subexpr (p/pseq-at 2 "(" :ws :expr :ws ")")
+```
+
+which parses the parens and optional whitespace, but returns just the inner
+`:expr`.
+
+
+### Actions
+
+Very often, the raw values as returned by the parser are not the values we want
+in our AST. For example, we want numeric literals as numbers, not as strings or
+lists of characters.
+
+This is achieved in a composable way by wrapping an inner parser with
+`p/with-action`. This runs the inner parser, then feeds its results through a
+function you provide, which becomes the value of the `p/with-action`.
+
+So we might refine the `:number` literal parser above to be:
+
+```clojure
+  :number (p/with-action
+            (p/many1 (p/span \0 \9))
+            (fn [chars _]
+              (Integer/parseInt (apply str chars))))
+```
+
+The second argument gives some information about the parsing context, such as
+the location in the file for error messages. See the docs for `p/with-action`
+for details.
+
+
+### Example: JSON Parser
+
+There's a complete JSON parser in
+[`json_test.clj`](/shepheb/parka/tree/main/test/parka/json_test.clj) that should
+serve as a nice example.
+
--- test/parka/json_test.clj
+++ test/parka/json_test.clj
@@ -1,7 +1,7 @@
 (ns parka.json-test
   "Test that builds and exercises a JSON parser using this library."
   (:require
-    [clojure.test :refer [deftest is]]
+    [clojure.test :refer [deftest is testing]]
     [parka.core :as p]))
 
 (def json-rules
@@ -12,16 +12,43 @@
    :bool       (p/with-action
                  (p/alt "true" "false")
                  (fn [v _] (= v "true")))
+   :hex        (p/one-of "0123456789ABCDEFabcdef")
+   :unicode    (p/with-action (p/pseq "\\u" :hex :hex :hex :hex)
+                 (fn [[_ a b c d] _]
+                   (char (Long/parseLong (str a b c d) 16))))
+   :escaped    (p/with-action (p/pseq-at 1 "\\" (p/one-of "\"\\/nrbft"))
+                 (fn [v _]
+                   (case v
+                     \n \newline
+                     \r \return
+                     \b \backspace
+                     \f \formfeed
+                     \t \tab
+                     v)))
+   :strchar    (p/alt :unicode :escaped p/any)
    :string     (p/stringify (p/pseq-at 1 "\""
-                                      (p/many-till p/any "\"")))
+                                       (p/many-till :strchar "\"")))
+
+   :pm         (p/one-of "+-")
+   :digits     (p/stringify (p/many1 (p/span \0 \9)))
    :number     (p/with-action
-                 (p/pseq (p/optional (p/one-of "+-"))
-                         (p/stringify (p/many1 (p/span \0 \9))))
-                 (fn [[sign s] _]
-                   (let [n (Integer/parseInt s)]
+                 (p/pseq (p/optional :pm) :digits
+                         (p/optional (p/pseq-at 1 "." :digits))
+                         (p/optional (p/pseq (p/lit-ic "e")
+                                             (p/optional :pm)
+                                             :digits)))
+                 (fn [[sign main d [_ em e]] _]
+                   (let [n (Integer/parseInt main)
+                         pos (if (or d e)
+                               (Double/parseDouble
+                                 (str n
+                                      (if d (str "." d) "")
+                                      (if e (str "e" (or em "") e) "")))
+                               (Integer/parseInt main))]
                      (if (= \- sign)
-                       (- n)
-                       n))))
+                       (- pos)
+                       pos))))
+
    :comma      (p/pseq :ws "," :ws)
    :object     (p/with-action
                  (p/between
@@ -45,11 +72,16 @@
   (p/parse-str (p/grammar json-rules sym) "<test>" input))
 
 (deftest test-number
-  (is (= 1   (test-parse-from :number "1")))
-  (is (= 123 (test-parse-from :number "0123")))
-  (is (= 0   (test-parse-from :number "0")))
-  (is (= 88  (test-parse-from :number "+88")))
-  (is (= -41 (test-parse-from :number "-41"))))
+  (is (= 1        (test-parse-from :number "1")))
+  (is (= 123      (test-parse-from :number "0123")))
+  (is (= 0        (test-parse-from :number "0")))
+  (is (= 88       (test-parse-from :number "+88")))
+  (is (= -41      (test-parse-from :number "-41")))
+  (is (= 0.02     (test-parse-from :number "0.02")))
+  (is (= 200.0    (test-parse-from :number "0.02e4")))
+  (is (= 123.4e-2 (test-parse-from :number "123.4e-2")))
+  (is (= 0.12     (test-parse-from :number "12e-2")))
+  )
 
 (deftest test-ws
   (is (= nil (test-parse-from :ws "   \t\t \n   \t"))))
@@ -58,8 +90,23 @@
   (is (= [nil "," nil] (test-parse-from :comma "    \t  ,\n   "))))
 
 (deftest test-string
-  (is (= "asdf" (test-parse-from :string "\"asdf\"")))
-  (is (= ""     (test-parse-from :string "\"\""))))
+  (testing "basic strings"
+    (is (= "asdf" (test-parse-from :string "\"asdf\"")))
+    (is (= ""     (test-parse-from :string "\"\""))))
+
+  (testing "escapes"
+    (is (= \\         (test-parse-from :strchar "\\\\")))
+    (is (= \"         (test-parse-from :strchar "\\\"")))
+    (is (= \newline   (test-parse-from :strchar "\\n")))
+    (is (= \return    (test-parse-from :strchar "\\r")))
+    (is (= \formfeed  (test-parse-from :strchar "\\f")))
+    (is (= \backspace (test-parse-from :strchar "\\b")))
+    (is (= \tab       (test-parse-from :strchar "\\t"))))
+  (testing "unicode"
+    (is (= \uBEEF (test-parse-from :strchar "\\ubeef"))))
+  (testing "strings with escapes"
+    (is (= "\n"   (test-parse-from :string "\"\\n\"")))
+    (is (= "\\"   (test-parse-from :string "\"\\\\\"")))))
 
 (deftest test-bool
   (is (= false (test-parse-from :bool "false")))
@@ -95,4 +142,13 @@
                        "def"  "yolo"
                        "asdf" nil}]
          (test-parse "  [ 1  ,false,null , {  \"abc\"  :true, \"def\": \"yolo\", \"asdf\": null  }  ]  "))))
+
+
+(comment
+  ; Running this JSON parser on the 7MB payload used by
+  ; https://github.com/GoogleChromeLabs/json-parse-benchmark
+  ; This times 100 runs.
+  (time (test-parse (slurp "./inspector-json-payload.json")))
+  (time (test-parse (slurp "./pp.json")))
+  )
 
